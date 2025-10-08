90f990254dd22208d3acf2cbbdad84284c02e37e
Emir Bugra Koksalan <kodmanyagha@gmail.com>
2025-10-09 00:02:46 +0300

    comment added

--- 4-control/406-compound-interest.c
+++ 4-control/406-compound-interest.c
@@ -1,6 +1,18 @@
 #include <math.h>
 #include <stdio.h>
 
+/*
+Bir kişi $1000'ını %5 faizle bankaya yatırmıştır. Bütün faizin hesaptaki
+paraya eklendiğini düşünerek 10 yıl boyunca her yıl sonunda hesapta birikecek
+parayı hesaplayıp yazdırınız. Aşağıdaki formülü kullanın:
+
+  a = p(1 + r) ^ n
+  a: n yıl sonra hesapta birikecek miktar
+  p: ilk yatırılan miktar
+  n: yıl sayısı
+  r: faiz oranı
+*/
+
 int main() {
   int yil;
   double miktar, anapara = 1000.0, oran = .05;
